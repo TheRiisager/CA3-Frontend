18d3f3b41bd02ae37d048639e98dd9c32ce9e453
Frederik Riisager Johnsen <riisager@pop-os.localdomain>
2020-11-13 18:26:30 +0100

    first commit

---
+++ src/settings.js
@@ -0,0 +1,10 @@
+function makeSettings() {
+    const URL = "http://localhost:8080/ca3backend_war";
+
+    return {
+        URL
+    }
+}
+
+const Settings = makeSettings();
+export default Settings;
